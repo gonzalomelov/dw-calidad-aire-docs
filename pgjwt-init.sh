d983bdfbb1e2834f60e230f607f3c48900e71473
Gonzalo Melo <gonzalo@teamkona.io>
2019-03-16 11:14:03 -0300

    Remove unused docker-compose files

--- pgjwt-init.sh
+++
@@ -1,7 +0,0 @@
-apt-get update
-apt-get --assume-yes install postgresql-server-dev-9.6
-apt-get --assume-yes install build-essential
-apt-get --assume-yes install git
-git clone https://github.com/michelp/pgjwt.git
-cd pgjwt
-make install
