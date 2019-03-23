54c2b30afd321b24137442292a78ac7e4a631744
Gonzalo Melo <gonzalo@teamkona.io>
2019-03-23 16:14:55 -0300

    Add more staging tables

---
+++ 4. Diseño físico/staging/diasimportantes.sql
@@ -0,0 +1,88 @@
+-- Sequence: data.diasimportantes_id_seq
+
+-- DROP SEQUENCE data.diasimportantes_id_seq;
+
+CREATE SEQUENCE data.diasimportantes_id_seq
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 7
+  CACHE 1;
+ALTER TABLE data.diasimportantes_id_seq
+  OWNER TO superuser;
+GRANT ALL ON SEQUENCE data.diasimportantes_id_seq TO superuser;
+GRANT USAGE ON SEQUENCE data.diasimportantes_id_seq TO webuser;
+
+
+-- Table: data.diasimportantes
+
+-- DROP TABLE data.diasimportantes;
+
+CREATE TABLE data.diasimportantes
+(
+  id integer NOT NULL DEFAULT nextval('diasimportantes_id_seq'::regclass),
+
+  iddiaimportante integer NOT NULL,
+  nomdiaimportante character varying(20),
+  idtipodiaimportante integer,
+  nomtipodiaimportante character varying(20),
+  idfecha integer,
+  idhora integer,
+
+  private boolean DEFAULT false,
+  owner_id integer DEFAULT request.user_id(),
+  CONSTRAINT diasimportantes_pkey PRIMARY KEY (id),
+  CONSTRAINT diasimportantes_owner_id_fkey FOREIGN KEY (owner_id)
+      REFERENCES data."user" (id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE data.diasimportantes
+  OWNER TO superuser;
+GRANT ALL ON TABLE data.diasimportantes TO superuser;
+GRANT SELECT, UPDATE, INSERT, DELETE ON TABLE data.diasimportantes TO api;
+
+-- Trigger: send_change_event on data.diasimportantes
+
+-- DROP TRIGGER send_change_event ON data.diasimportantes;
+
+CREATE TRIGGER send_change_event
+  AFTER INSERT OR UPDATE OR DELETE
+  ON data.diasimportantes
+  FOR EACH ROW
+  EXECUTE PROCEDURE rabbitmq.on_row_change('{"include":["id","nomdiaimportante"]}');
+
+-- INSERT
+
+INSERT INTO data.diasImportantes(id, idDiaImportante, nomDiaImportante, idTipoDiaImportante, nomTipoDiaImportante, idFecha, idHora, private, owner_id) VALUES (1, 0, 'No es día importante', 0, 'No es día importante', null, null, false, 1);
+INSERT INTO data.diasImportantes(id, idDiaImportante, nomDiaImportante, idTipoDiaImportante, nomTipoDiaImportante, idFecha, idHora, private, owner_id) VALUES (2, 1, 'Uruguay y Portugal', 1, 'Mundial', 20180630, 15, false, 1);
+INSERT INTO data.diasImportantes(id, idDiaImportante, nomDiaImportante, idTipoDiaImportante, nomTipoDiaImportante, idFecha, idHora, private, owner_id) VALUES (3, 2, '1ro de Mayo', 2, 'Fecha Patria', 20160501, 13, false, 1);
+
+-- ALTER SEQUENCE
+
+ALTER SEQUENCE data.diasimportantes_id_seq RESTART WITH 4;
+
+-- View: api.diasImportantes
+
+-- DROP VIEW api.diasImportantes;
+
+CREATE OR REPLACE VIEW api.diasImportantes AS 
+ SELECT diasImportantes.id,
+
+    diasImportantes.iddiaimportante,
+    diasImportantes.nomdiaimportante,
+    diasImportantes.idtipodiaimportante,
+    diasImportantes.nomtipodiaimportante,
+    diasImportantes.idfecha,
+    diasImportantes.idhora,
+
+    diasImportantes.private,
+    diasImportantes.owner_id = request.user_id() AS mine
+   FROM diasImportantes;
+
+ALTER TABLE api.diasImportantes
+  OWNER TO api;
+GRANT ALL ON TABLE api.diasImportantes TO api;
+GRANT SELECT, UPDATE, INSERT, DELETE ON TABLE api.diasImportantes TO webuser;
