915f84d5fd82eb28b97da7785952f4884ed0cce3
Gonzalo Melo <gonzalo@kona.tech>
2019-11-21 16:29:20 -0300

    Enable desktop apps

--- start.sh
+++ start.sh
@@ -3,6 +3,8 @@ osascript proygrad-finder.applescript
 subl "/Users/gonzalomelo/Kona/proy-grad/Proyecto Calidad del Aire"
 gittower "/Users/gonzalomelo/Kona/proy-grad/Proyecto Calidad del Aire"
 osascript proygrad-terminal.applescript
+xattr -dr com.apple.quarantine 'Data Integration.app'
+xattr -dr com.apple.quarantine 'Schema_Workbench.app'
 open -a 'Data Integration'
 open -a 'pgAdmin3'
 open -a 'Schema_Workbench'
